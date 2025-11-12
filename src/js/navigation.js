6f50ae7c240e409c477570de02d2d37eba182fa7
Mehrab <mmehrab.pk@gmail.com>
2025-11-12 08:41:18 +0330

    update

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,30 +1,30 @@
 const nav = document.querySelectorAll(".navbar");
 const pages = document.querySelectorAll(".page");
 
-// nav.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     let name = item.dataset.name;
-//     nav.forEach(item=>item.classList.remove('nav-active'))
-//     item.classList.add('nav-active')
-//     pages.forEach((page) => {
-//       page.classList.add("hidden");
-//       if (name == page.dataset.name) {
-//         page.classList.remove("hidden");
-//       }
-//     });
+ nav.forEach((item) => {
+   item.addEventListener("click", () => {
+     let name = item.dataset.name;
+     nav.forEach(item=>item.classList.remove('nav-active'))
+     item.classList.add('nav-active')
+     pages.forEach((page) => {
+       page.classList.add("hidden");
+       if (name == page.dataset.name) {
+         page.classList.remove("hidden");
+       }
+     });
+   });
+ });
+
+// const showPage = (name) => {
+//   nav.forEach((item) => {
+//     item.classList.toggle("nav-active", item.dataset.name === name);
 //   });
+
+//   pages.forEach((page) => {
+//     page.classList.toggle("hidden", page.dataset.name === name);
+//   });
+// };
+
+// nav.forEach((item) => {
+//   item.addEventListener("click", () => {showPage(item.dataset.name)});
 // });
-
-const showPage = (name) => {
-  nav.forEach((item) => {
-    item.classList.toggle("nav-active", item.dataset.name === name);
-  });
-
-  pages.forEach((page) => {
-    page.classList.toggle("hidden", page.dataset.name === name);
-  });
-};
-
-nav.forEach(item=>{
-    item.addEventListener('click', ()=> showPage(item.dataset.name))
-})
